7941a1981836606b116495071c5e5680d459f7ee
ANAcfg <80727432+ANAcfg@users.noreply.github.com>
2021-09-17 16:54:18 -0400

    Part2

---
+++ class1/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Ana Fuentes on 9/17/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
